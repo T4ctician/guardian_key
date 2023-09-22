c467a0fc7d49e6153c4d7da8a0ed672e0f6d0618
kennytan94 <kennytwk94@hotmail.com>
2023-09-22 13:42:23 +0800

    Const color template

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+
+
+// -- ON-BOARDING COLORS
+const tOnBoardingPage1Color = Colors.white;
+const tOnBoardingPage2Color = Color(0xfffddcdf);
+const tOnBoardingPage3Color = Color(0xffffdcbd);
