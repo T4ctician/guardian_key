bcc9461a26929e35cebfdf982daa4284428db24a
kennytan94 <kennytwk94@hotmail.com>
2023-09-22 13:45:31 +0800

    Added image strings

---
+++ lib/src/constants/image_strings.dart
@@ -0,0 +1,6 @@
+/* -- App Image Strings -- */
+
+// -- OnBoarding Screen Images
+const String tOnBoardingImage1 = "assets/images/on_boarding_images/on-boarding-image-1.png";
+const String tOnBoardingImage2 = "assets/images/on_boarding_images/on-boarding-image-2.png";
+const String tOnBoardingImage3 = "assets/images/on_boarding_images/on-boarding-image-3.png";
